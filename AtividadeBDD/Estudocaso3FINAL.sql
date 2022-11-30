cca65ba37147589dec803208bd861b5b7f291e1e
Bernardo <benckebernardo@gmail.com>
2022-11-30 08:47:00 -0300

    Inserts e deletes editados

--- AtividadeBDD/Estudocaso3FINAL.sql
+++ AtividadeBDD/Estudocaso3FINAL.sql
@@ -84,17 +84,6 @@ insert into pagamentos (forma_pagamento, pagamento_data, reembolso) values ('Pay
 	insert into pagamentos (forma_pagamento, pagamento_data, reembolso) values ('PayPal', '2020/02/02', false);
 	insert into pagamentos (forma_pagamento, pagamento_data, reembolso) values ('Crédito', '2019/03/10', true);
     
-insert into jogos (faixa_etaria, avaliacao, preco, jogo_nome) values ('14', 0.2, 158.23, 'Tresom');
-	insert into jogos (faixa_etaria, avaliacao, preco, jogo_nome) values ('14', 2.6, 360.93, 'Home Ing');
-	insert into jogos (faixa_etaria, avaliacao, preco, jogo_nome) values ('10', 3.1, 367.96, 'Matsoft');
-	insert into jogos (faixa_etaria, avaliacao, preco, jogo_nome) values ('12', 1.4, 190.21, 'Y-Solowarm');
-	insert into jogos (faixa_etaria, avaliacao, preco, jogo_nome) values ('14', 4.5, 380.75, 'Sonsing');
-	insert into jogos (faixa_etaria, avaliacao, preco, jogo_nome) values ('L', 3.7, 93.57, 'Flexidy');
-	insert into jogos (faixa_etaria, avaliacao, preco, jogo_nome) values ('14', 2.2, 152.04, 'Andalax');
-	insert into jogos (faixa_etaria, avaliacao, preco, jogo_nome) values ('12', 4.8, 118.27, 'Z-Solowarm');
-	insert into jogos (faixa_etaria, avaliacao, preco, jogo_nome) values ('10', 1.2, 420.73, 'Bytecard');
-	insert into jogos (faixa_etaria, avaliacao, preco, jogo_nome) values ('14', 4.4, 134.59, 'Solarbreeze');
-
 insert into usuarios (email, senha, apelido) values ('rfosten0@webeden.co.uk', 'CzXOs3UD5Oaa', 'ebeddie0');
 	insert into usuarios (email, senha, apelido) values ('jbrogden1@goodreads.com', 'p7pv6UbSH', 'eforce1');
 	insert into usuarios (email, senha, apelido) values ('thardson2@bizjournals.com', 'gU24YP', 'saulton2');
@@ -106,6 +95,17 @@ insert into usuarios (email, senha, apelido) values ('rfosten0@webeden.co.uk', '
 	insert into usuarios (email, senha, apelido) values ('atrevithick8@mapy.cz', 'jDqxuU9ivvR', 'cpettwood8');
 	insert into usuarios (email, senha, apelido) values ('mpoppy9@samsung.com', 'tjlfIJHXo', 'vcarous9');
     
+insert into jogos (faixa_etaria, avaliacao, preco, jogo_nome) values ('14', 0.2, 158.23, 'Tresom');
+	insert into jogos (faixa_etaria, avaliacao, preco, jogo_nome) values ('14', 2.6, 360.93, 'Home Ing');
+	insert into jogos (faixa_etaria, avaliacao, preco, jogo_nome) values ('10', 3.1, 367.96, 'Matsoft');
+	insert into jogos (faixa_etaria, avaliacao, preco, jogo_nome) values ('12', 1.4, 190.21, 'Y-Solowarm');
+	insert into jogos (faixa_etaria, avaliacao, preco, jogo_nome) values ('14', 4.5, 380.75, 'Sonsing');
+	insert into jogos (faixa_etaria, avaliacao, preco, jogo_nome) values ('L', 3.7, 93.57, 'Flexidy');
+	insert into jogos (faixa_etaria, avaliacao, preco, jogo_nome) values ('14', 2.2, 152.04, 'Andalax');
+	insert into jogos (faixa_etaria, avaliacao, preco, jogo_nome) values ('12', 4.8, 118.27, 'Z-Solowarm');
+	insert into jogos (faixa_etaria, avaliacao, preco, jogo_nome) values ('10', 1.2, 420.73, 'Bytecard');
+	insert into jogos (faixa_etaria, avaliacao, preco, jogo_nome) values ('14', 4.4, 134.59, 'Solarbreeze');
+
 insert into desenvolvedora (biblioteca_jogos, desenvolvedora_nome, desenvolvedora_regiao, premiacoes) values ('Andalax', 'Toughjoyfax', 'TZ', 'Best Adaptation');
 	insert into desenvolvedora (biblioteca_jogos, desenvolvedora_nome, desenvolvedora_regiao, premiacoes) values ('Ventosanzap', 'Voyatouch', 'PA', 'Best Adaptation');
 	insert into desenvolvedora (biblioteca_jogos, desenvolvedora_nome, desenvolvedora_regiao, premiacoes) values ('Duobam', 'Mat Lam Tam', 'CZ', 'Best Adaptation');
@@ -117,29 +117,29 @@ insert into desenvolvedora (biblioteca_jogos, desenvolvedora_nome, desenvolvedor
 	insert into desenvolvedora (biblioteca_jogos, desenvolvedora_nome, desenvolvedora_regiao, premiacoes) values ('Rank', 'Greenlam', 'RU', 'Best Adaptation');
 	insert into desenvolvedora (biblioteca_jogos, desenvolvedora_nome, desenvolvedora_regiao, premiacoes) values ('Prodder', 'Flowdesk', 'RU', null);
     
-update usuarios set senha = 'HAk1ucjjb', apelido = 'cpettwood8' where email = 'mpoppy9@samsung.com';
-	update usuarios set senha = 'tjlfIJHXo', apelido = 'ntomowicz7' where email = 'trioch5@geocities.com';
-    update usuarios set senha = 'jDqxuU9ivvR', apelido = 'vcarous9' where email = 'atrevithick8@mapy.cz';
-    update usuarios set senha = '4GxnMe', apelido = 'drodenhurst6' where email = 'jbrogden1@goodreads.com';
-    update usuarios set senha = 'CzXOs3UD5Oaa', apelido = 'eforce1' where email = 'thardson2@bizjournals.com';
+update usuarios set senha = 'HAk1ucjjb', apelido = 'cpettwood8' where usuarios_id = 18;
+	update usuarios set senha = 'tjlfIJHXo', apelido = 'ntomowicz7' where usuarios_id = 2348;
+    update usuarios set senha = 'jDqxuU9ivvR', apelido = 'vcarous9' where usuarios_id = 88345;
+    update usuarios set senha = '4GxnMe', apelido = 'drodenhurst6' where usuarios_id = 7772;
+    update usuarios set senha = 'CzXOs3UD5Oaa', apelido = 'eforce1' where usuarios_id = 92471;
     
-update pagamentos set forma_pagamento = Crédito, reembolso = false where pagamento_data = '2022/08/20';
-	update pagamentos set forma_pagamento = Débito, reembolso = false where pagamento_data = '2020/10/06';
-    update pagamentos set forma_pagamento = Pix, reembolso = false where pagamento_data = '2017/02/03';
-    update pagamentos set forma_pagamento = Débito, reembolso = true where pagamento_data = '2019/03/10';
-    update pagamentos set forma_pagamento = PayPal, reembolso = false where pagamento_data = '2021/09/26';
+update pagamentos set forma_pagamento = 'Crédito', reembolso = false where pagamento_id = 9934;
+	update pagamentos set forma_pagamento = 'Débito', reembolso = false where pagamento_id = 88233;
+    update pagamentos set forma_pagamento = 'Pix', reembolso = false where pagamento_id = 956;
+    update pagamentos set forma_pagamento = 'Débito', reembolso = true where pagamento_id = 13473;
+    update pagamentos set forma_pagamento = 'PayPal', reembolso = false where pagamento_id = 23775;
     
-update desenvolvedora set biblioteca_jogos = 'Holdlamis', desenvolvedora_regiao = 'CZ' where desenvolvedora_nome = 'Flowdesk';
-	update desenvolvedora set biblioteca_jogos = 'Sonair', desenvolvedora_regiao = 'US' where desenvolvedora_nome = 'Overhold';
-    update desenvolvedora set biblioteca_jogos = 'Andalax', desenvolvedora_regiao = 'BR' where desenvolvedora_nome = 'Domainer';
-    update desenvolvedora set biblioteca_jogos = 'Ventosanzap', desenvolvedora_regiao = 'UK' where desenvolvedora_nome = 'Toughjoyfax';
-    update desenvolvedora set biblioteca_jogos = 'Zamit', desenvolvedora_regiao = 'JP' where desenvolvedora_nome = 'Bigtax';
+update desenvolvedora set biblioteca_jogos = 'Holdlamis', desenvolvedora_regiao = 'CZ' where desenvolvedora_id = 64;
+	update desenvolvedora set biblioteca_jogos = 'Sonair', desenvolvedora_regiao = 'US' where desenvolvedora_id = 324;
+    update desenvolvedora set biblioteca_jogos = 'Andalax', desenvolvedora_regiao = 'BR' where desenvolvedora_id = 84;
+    update desenvolvedora set biblioteca_jogos = 'Ventosanzap', desenvolvedora_regiao = 'UK' where desenvolvedora_id = 29;
+    update desenvolvedora set biblioteca_jogos = 'Zamit', desenvolvedora_regiao = 'JP' where desenvolvedora_id = 1108;
     
-update jogos set avaliacao = 4.8, preco = 420.73 where jogo_nome = 'Bytecard';
-	update jogos set avaliacao = 3.9, preco = 65.91 where jogo_nome = 'Z-Solowarm';
-    update jogos set avaliacao = 3.8, preco = 10.00 where jogo_nome = 'Solarbreeze';
-    update jogos set avaliacao = 2.0, preco = 43.98 where jogo_nome = 'Tresom';
-    update jogos set avaliacao = 4.1, preco = 2.50 where jogo_nome = 'Home Ing';
+update jogos set avaliacao = 4.8, preco = 420.73 where jogos_id = 726;
+	update jogos set avaliacao = 3.9, preco = 65.91 where jogos_id = 9993;
+    update jogos set avaliacao = 3.8, preco = 10.00 where jogos_id = 52;
+    update jogos set avaliacao = 2.0, preco = 43.98 where jogos_id = 635;
+    update jogos set avaliacao = 4.1, preco = 2.50 where jogos_id = 8245;
     
 delete from usuarios where usuarios_id = 8933;
 	delete from usuarios where usuarios_id = 4327;
